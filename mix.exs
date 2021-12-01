0f2939d741c7823f2e5a9d5f1507663193601aa3
AndrzejPodo <andrzejpodobinski2@gmail.com>
2021-12-01 15:32:21 +0100

    bump to 0.7.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.RTP.H264.MixProject do
   use Mix.Project
 
-  @version "0.6.0"
+  @version "0.7.0"
   @github_url "https://github.com/membraneframework/membrane_rtp_h264_plugin"
 
   def project do
