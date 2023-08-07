20e98ec183fec2f08e0e1b3238b1f62663870896
Jakub Pisarek <99591440+sgfn@users.noreply.github.com>
2023-08-07 16:47:29 +0200

    Bump minor

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.RTP.H264.MixProject do
   use Mix.Project
 
-  @version "0.16.1"
+  @version "0.17.0"
   @github_url "https://github.com/membraneframework/membrane_rtp_h264_plugin"
 
   def project do
