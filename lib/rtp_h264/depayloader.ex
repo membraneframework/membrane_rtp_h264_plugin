883db95e7b6c5be30d70ba16935b172b3e04a9bb
Noarkhh <jakub.pryc@gmail.com>
2023-09-04 16:01:39 +0200

    Make H264 accepted_formats more specific

--- lib/rtp_h264/depayloader.ex
+++ lib/rtp_h264/depayloader.ex
@@ -21,7 +21,7 @@ defmodule Membrane.RTP.H264.Depayloader do
   def_input_pad :input, accepted_format: RTP, demand_mode: :auto
 
   def_output_pad :output,
-    accepted_format: %H264{alignment: :nalu},
+    accepted_format: %H264{alignment: :nalu, stream_structure: :annexb},
     demand_mode: :auto
 
   defmodule State do
