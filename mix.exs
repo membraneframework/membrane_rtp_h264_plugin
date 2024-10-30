6037da5528e2c8e112d29446468ff73671303c62
Mateusz Front <mateusz.front@swmansion.com>
2024-10-30 10:22:35 +0100

    bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.RTP.H264.MixProject do
   use Mix.Project
 
-  @version "0.19.4"
+  @version "0.20.0"
   @github_url "https://github.com/membraneframework/membrane_rtp_h264_plugin"
 
   def project do
