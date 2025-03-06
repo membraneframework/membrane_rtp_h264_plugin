c5bb8fb49b74c88c63ea848caf6ce8d848fa6b81
noarkhh <jakub.pryc@gmail.com>
2025-03-06 15:08:55 +0100

    Fix payload_format

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.RTP.H264.MixProject do
   use Mix.Project
 
-  @version "0.20.3"
+  @version "0.20.4"
   @github_url "https://github.com/membraneframework/membrane_rtp_h264_plugin"
 
   def project do
